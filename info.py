7143a05e643e17185500cc73e0ba28934e8c5c25
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-04-08 23:48:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8
 CODE = (environ.get('CODE', 'https://telegra.ph/file/96c601be23848f648bdb4.jpg'))
 
 #stream link shortner
-    STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
+STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
 STREAM_API = (environ.get('STREAM_API', 'ae0832f39e24094a0661626a792e6a2b8880e310'))
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/HowToOpenLinkSMK'))
 
